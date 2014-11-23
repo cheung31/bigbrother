ceeca2618214d2422050716609709b386c423632
Ryan Cheung <cheung31@gmail.com>
2014-11-23 00:54:04 -0500

    Add rssi stuff

---
+++ app/controllers/rssi.server.controller.js
@@ -0,0 +1,54 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	errorHandler = require('./errors.server.controller'),
+	Rssi = mongoose.model('Rssi'),
+	_ = require('lodash');
+
+/**
+ * Create a rssi
+ */
+exports.create = function(req, res) {
+    if (req.body.length) {
+        Rssi.collection.insert(req.body, {}, function(err) {
+            if (err) {
+                return res.status(400).send({
+                    message: errorHandler.getErrorMessage(err)
+                });
+            } else {
+                res.json(rssi);
+            }
+        });
+    } else {
+	    var rssi = new Rssi(req.body[i]);
+        rssi.save(function(err) {
+            if (err) {
+                return res.status(400).send({
+                    message: errorHandler.getErrorMessage(err)
+                });
+            } else {
+                res.json(rssi);
+            }
+        });
+    }
+};
+
+/**
+ * List of Visitors
+ */
+exports.list = function(req, res) {
+    Rssi.find()
+        .sort('-created')
+        .exec(function(err, rssis) {
+	        if (err) {
+	        	return res.status(400).send({
+	        		message: errorHandler.getErrorMessage(err)
+	        	});
+	        } else {
+	        	res.json(rssis);
+	        }
+	    });
+};
