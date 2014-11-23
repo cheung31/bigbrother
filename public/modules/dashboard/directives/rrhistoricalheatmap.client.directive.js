e6f6ba9babd589acd2aed280f6ce2ba2d86f46fe
Ryan Cheung <cheung31@gmail.com>
2014-11-23 16:12:42 -0500

    Remove 10m cap on real-time. Fix sizing to historical map

--- public/modules/dashboard/directives/rrhistoricalheatmap.client.directive.js
+++ public/modules/dashboard/directives/rrhistoricalheatmap.client.directive.js
@@ -7,21 +7,27 @@ angular.module('dashboard').directive('retailRocketHistoricalHeatmap', ['Visitor
 			scope: {},
 			replace: true,
 			link: function (scope, element, attrs) {
+				// Constants
+				var	pixelToMapYRatio = 575 / 751;
+				var pixelsPerMetre = 150 / 8.80872;
+				var originXpixel = 400,
+					originYpixel = 100;
+
 				scope.find = function() {
-					scope.visitors = Visitors.historical.query({
-                        since: 123
-                    });
+					scope.visitors = Visitors.all.query();
 				};
 
 				var svg = d3.select(element[0])
 				            .append('svg')
-				            .style('width', 600)
-				            .style('height', 751);
+				            .attr('width', 600)
+				            .attr('height', 575)
+				            .attr('x', 0)
+				            .attr('y', 0);
 
 			    var floorplan = svg.append('svg:image')
 											.attr('xlink:href', 'http://i.imgur.com/czBvsER.png')
 											.attr('width', 600)
-											.attr('height', 751)
+											.attr('height', 575)
 											.attr('x', 0)
 											.attr('y', 0);
 
@@ -42,12 +48,18 @@ angular.module('dashboard').directive('retailRocketHistoricalHeatmap', ['Visitor
 					var circles = svg.selectAll('circle')
 							                  .data(scope.visitors)
 							                  .enter()
-							                  .append('circle');
+							                  .append('circle')
+							                  .filter(function (d) { return d.x > 0});
 
-					var circleAttributes = circles.attr('cx', function (d) { return d.x; })
-							                      .attr('cy', function (d) { return d.y; })
-							                      .attr('r', 10)
-							                      .style('fill', 'red');
+					// var circleattributes = circles.attr('cx', function (d) { return originxpixel; })
+					// 		                      .attr('cy', function (d) { return originypixel * pixeltomapyratio; })
+					// 		                      .attr('r', 10)
+					// 		                      .style('fill', 'red');
+					var circleattributes = circles.attr('cx', function (d) { return (d.x / 100) * pixelspermetre + originxpixel; })
+							                      .attr('cy', function (d) { return ((d.y / 100) * pixelspermetre + originypixel) * pixeltomapyratio; })
+							                      .attr('r', 8)
+							                      .style('fill', 'red')
+							                      .style('opacity', 0.5);
 				};
 			}
 		};
