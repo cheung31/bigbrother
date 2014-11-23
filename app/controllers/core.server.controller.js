81c281f4b4d9fd146e3d0154bbf1e98ca3168337
Ryan Cheung <cheung31@gmail.com>
2014-11-22 21:22:31 -0500

    Add landing page

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -4,8 +4,9 @@
  * Module dependencies.
  */
 exports.index = function(req, res) {
-	res.render('index', {
-		user: req.user || null,
-		request: req
-	});
+	res.render('index');
+};
+
+exports.dashboard = function(req, res) {
+	res.render('dashboard');
 };
