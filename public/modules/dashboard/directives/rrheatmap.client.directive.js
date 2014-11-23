cb8da4423f430ac62bc9e39dd16cc9c565b2b330
Emmanuel Lee <eman.j.lee@gmail.com>
2014-11-23 07:53:39 -0500

    Added heatmap directive with interval polling.

--- public/modules/dashboard/directives/rrheatmap.client.directive.js
+++ public/modules/dashboard/directives/rrheatmap.client.directive.js
@@ -1,33 +1,53 @@
 'use strict';
 
-angular.module('dashboard').directive('retailRocketHeatmap',
-	function() {
+angular.module('dashboard').directive('retailRocketHeatmap', ['Visitors',
+	function(Visitors) {
 		return {
 			restrict: 'E',
-			scope: {visitors: '='},
+			scope: {},
+			replace: true,
 			link: function (scope, element, attrs) {
-				var heatmap = d3.select(element[0]);
+				scope.find = function() {
+					scope.visitors = Visitors.query();
+				}
 
-				var svgContainer = heatmap.append('svg')
-										  .attr('width', 600)
-				       					  .attr('height', 751);
+				var svg = d3.select(element[0])
+				            .append('svg')
+				            .style('width', 600)
+				            .style('height', 751);
 
-			    var floorplan = svgContainer.append('svg:image')
+			    var floorplan = svg.append('svg:image')
 											.attr('xlink:href', 'http://i.imgur.com/czBvsER.png')
 											.attr('width', 600)
-											.attr('height', 751);
+											.attr('height', 751)
+											.attr('x', 0)
+											.attr('y', 0);
 
-				var circles = floorplan.selectAll('circle')
-						                  .data(scope.visitors)
-						                  .enter()
-						                  .append('circle');
+	            // watch for data changes and re-render on changes
+				scope.$watch('visitors', function (newVals, oldVals) {
+					if (newVals) {
+						return scope.render(newVals);
+					}
+				}, true);
 
-				var circleAttributes = circles.attr('cx', function (d) { return d.x; })
-						                      .attr('cy', function (d) { return d.y; })
-						                      .attr('r', 10)
-						                      .style('fill', 'red');
-				console.log(scope.visitors[0]);
+				setInterval(function() { scope.find() }, 1000);
+
+				// define render function
+				scope.render = function (data) {
+					// remove all previous items before render
+					svg.selectAll('circle').remove();
+
+					var circles = svg.selectAll('circle')
+							                  .data(scope.visitors)
+							                  .enter()
+							                  .append('circle');
+
+					var circleAttributes = circles.attr('cx', function (d) { return d.x; })
+							                      .attr('cy', function (d) { return d.y; })
+							                      .attr('r', 10)
+							                      .style('fill', 'red');
+				};
 			}
 		};
 	}
-);
+]);
