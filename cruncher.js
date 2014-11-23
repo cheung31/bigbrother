ee0972ac15629923d11776e5fb0385a80030001c
Dane Carr <dane.l.w.carr@gmail.com>
2014-11-23 07:14:38 -0500

    Numbers: now with 50% more crunch

--- cruncher.js
+++ cruncher.js
@@ -16,14 +16,21 @@ require('./app/models/rssi.model.js');
 require('./app/models/visitor.model.js');
 
 var Rssi = mongoose.model('Rssi');
+var Visitor = mongoose.model('Visitor');
+
+// Distances between routers in cm
+var d = 500,
+	i = 500,
+	j = 500;
 
 Rssi.find({'processed': false}).exec(function(err, rssis) {
+
 	var macs = {};
 	var macTimeBuckets = {};
+	var macTimeNode = {};
 
 	// Break up by MAC address
 	rssis.forEach(function(rssi) {
-		rssi.distance = Math.pow(10, -(rssi.rssi + 2.425) / 23.28);
 		if (!macs[rssi.mac]) macs[rssi.mac] = [];
 		macs[rssi.mac].push(rssi);
 	});
@@ -32,15 +39,54 @@ Rssi.find({'processed': false}).exec(function(err, rssis) {
 	_.forIn(macs, function(value, key) {
 		macTimeBuckets[key] = {};
 		macs[key].forEach(function(rssi) {
-			var timeBucket = (new Date(Math.round(5 * Math.round(rssi.created / 5)) * 1000)).toString();
-			console.log(timeBucket)
+			var timeBucket = (new Date(Math.round(5 * Math.round(rssi.created / 5)))).toString();
 			if (!macTimeBuckets[key][timeBucket]) macTimeBuckets[key][timeBucket] = [];
 			macTimeBuckets[key][timeBucket].push(rssi);
 		});
 	});
 
-	
+	// Calculate approximate distances to each node/router
+	_.forIn(macTimeBuckets, function(value1, mac) {
+		macTimeNode[mac] = {};
+		_.forIn(macTimeBuckets[mac], function(value2, timeBucket) {
+			macTimeNode[mac][timeBucket] = {};
+			var totalDistance = {};
+			var count = {};
+			macTimeBuckets[mac][timeBucket].forEach(function(rssi) {
+				// approximate distance in cm
+				totalDistance[rssi.router] = (totalDistance[rssi.router] || 0) + Math.pow(10, -(rssi.rssi + 2.425) / 23.28);
+				count[rssi.router] = (count[rssi.router] || 0) + 1;
+			});
 
-	console.log(macTimeBuckets);
-	process.exit(0);
+			_.forIn(totalDistance, function(dist, router) {
+				if (count[router] > 0)
+					macTimeNode[mac][timeBucket][router] = dist / count[router];
+			});
+		});
+	});
+
+	var locations = [];
+
+	_.forIn(macTimeNode, function(value1, mac) {
+		_.forIn(macTimeNode[mac], function(value2, timeBucket) {
+			if (_.size(macTimeNode[mac][timeBucket]) >= 3) {
+				var x = (pow(value2['1'], 2) - pow(value2['2'], 2) + pow(d, 2)) / (2*d);
+				var y = ((pow(value2['1'], 2) - pow(value2['3'], 2) + pow(i, 2) + pow(j, 2)) / (2*j)) - (i*x/j);
+				var location = new Visitor({id: mac, created: timeBucket, x: x, y: y});
+				locations.push(location);
+			}
+		});
+	});
+
+	if (locations.length <= 0) {
+		console.log('No locations calculated');
+		process.exit(0);
+	}
+
+	Visitor.create(locations, function(err) {
+		if (err)
+			process.exit(1);
+		else
+			process.exit(0);
+	});
 });
--- packet_sniffer/pcapreader.py
+++ packet_sniffer/pcapreader.py
@@ -21,7 +21,7 @@ def main():
 			signal_strength = signal_strength - 256 if signal_strength > 127 else signal_strength
 			signal_strength = -signal_strength
 			print "[%d] MAC: %s RSSi: %d"%(pkt.time, pkt.addr1, signal_strength)
-			packets.append({'created': pkt.time, 'mac': pkt.addr1, 'rssi': signal_strength, 'router': routerId, 'processed': False})
+			packets.append({'created': pkt.time * 1000, 'mac': pkt.addr1, 'rssi': signal_strength, 'router': routerId, 'processed': False})
 			if len(packets) > 300:
 				thread = unirest.post("http://127.0.0.1:3000/rssi", headers = {"Content-Type": "application/json"}, params = json.dumps(packets), callback = callbackFunction)
 				packets = []
